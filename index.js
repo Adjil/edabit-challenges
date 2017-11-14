927bd50d383074472634f04b8f9aa250c2d79182
Ali Ilman <aliilman532@gmail.com>
2017-11-14 12:50:09 +0800

    Added solution file

---
+++ index.js
@@ -0,0 +1,7 @@
+//This is where my solutions live.
+
+//#1 Return The Last Item In An Array
+function getLastItem(arr) {
+  return arr.pop();
+}
+getLastItem(['React', 'Vue.js', 'jQuery', 'AngularJS', 'React Native']);
