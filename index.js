034c12e8cd29fed31b1748443f58bb09416ebe6b
Ali Ilman <aliilman532@gmail.com>
2017-11-15 11:27:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function isEvenOrOdd(num) {
   return num % 2 === 0 ? 'even' : 'odd';
 }
 isEvenOrOdd(15);
+
+//#3 Shuffle The Name
+function nameShuffle(str) {
+  return str.split(' ').reverse().join(' ');
+}
+nameShuffle('Richard Branson');
