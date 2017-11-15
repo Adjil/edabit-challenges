f55bac776b0cde83e5ce9df7fdc398e7b5a598d7
Ali Ilman <aliilman532@gmail.com>
2017-11-15 11:20:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function getLastItem(arr) {
   return arr.pop();
 }
 getLastItem(['React', 'Vue.js', 'jQuery', 'AngularJS', 'React Native']);
+
+//#2 Is The Number Even Or Odd?
+function isEvenOrOdd(num) {
+  return num % 2 === 0 ? 'even' : 'odd';
+}
+isEvenOrOdd(15);
